4bd76c87d696a8e0f70a08091b7d91134affd082
HervalMata <hervalmataamparo@gmail.com>
2020-03-30 18:33:02 -0300

    Criação do Projeto

---
+++ src/test/java/com/herval/microrestaurantes/MicroRestaurantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herval.microrestaurantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroRestaurantesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
